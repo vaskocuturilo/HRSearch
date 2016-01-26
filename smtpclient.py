486bd076df6b853b32ddf5be8a6ef86f5e52f2d5
Vasko Cuturilo <vaskocuturilo@rambler.ru>
2016-01-26 13:47:45 -0600

    New Version

--- smtpclient.py
+++ smtpclient.py
@@ -6,29 +6,30 @@ import string
 import random
 import sys, getopt
 from datetime import datetime
+fromaddres = sys.argv[1]
+toaddreses = sys.argv[2]
 def prompt(prompt):
     return raw_input(prompt).strip()
-def from_generator(size=3, chars=string.ascii_uppercase + string.digits):
+def from_generator(size=3, chars=string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
 def subject_generator(size=20, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
-def to_generator(size=3, chars=string.ascii_uppercase + string.digits):
+def to_generator(size=3, chars=string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
 def data_generator(size=30, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 def help():
  print 'Smtp Client v.1.0'
 def run():
- fromaddr =from_generator()+'@'
+ fromaddr =from_generator()+'@'+"".join(fromaddres)
  subject = subject_generator()
  line = data_generator()
  IP =  "IP: " + (socket.gethostbyname(socket.gethostname()))
  Date = datetime.now().strftime("%d-%m-%Y_%H_%S")
- toaddrs  = to_generator()+'@'
+ toaddrs  = to_generator()+'@'+"".join(toaddreses)
  msg = ("From: %s\r\nSubject: %s\r\nTo: %s\r\n"
        % (fromaddr, subject,toaddrs))
  msg = msg +IP+"\n"+Date+"\n"+"DATA: "+line
- print "Message length is " + repr(len(msg))
  server = smtplib.SMTP('localhost')
  server.set_debuglevel(1)
  server.sendmail(fromaddr,toaddrs,msg)
@@ -38,8 +39,8 @@ commands = {
     "--help" :help
 }
 if __name__ == "__main__":
-   try:
-      commands[" ".join(sys.argv[1:])]()
-   except KeyError:
-     print "Usage: --start for start service. Or --help for Help."
-      
+ #  try:
+  #    commands[" ".join(sys.argv[3:])]()
+  # except KeyError:
+  #   print "Usage: --start for start service. Or --help for Help."
+  run()

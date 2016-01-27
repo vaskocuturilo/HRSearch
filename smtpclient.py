5c5fcbdc98c12145d0f904e6f79cf0c2d646d8c1
Vasko Cuturilo <vaskocuturilo@rambler.ru>
2016-01-27 04:43:43 -0600

    New Version Smtp Client

--- smtpclient.py
+++ smtpclient.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 # -*- encoding: utf-8 -*-
 # Vasko Cuturuilo
+
 import smtplib
 import socket
 import time
@@ -11,6 +12,9 @@ import sys, getopt
 from datetime import datetime
 fromaddres = sys.argv[1:2]
 toaddreses = sys.argv[2:3]
+from ConfigParser import SafeConfigParser
+parser = SafeConfigParser()
+parser.read('config.ini')
 def prompt(prompt):
     return raw_input(prompt).strip()
 def from_generator(size=3, chars=string.ascii_lowercase):
@@ -23,6 +27,9 @@ def data_generator(size=30, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 def help():
  print 'Smtp Client v.1.0'
+ print 'Example: python smtpclient.py from.rambler.ru to.google.ru --start'
+def stop():
+ print 'Hey, what are you doing men ? Service is not running'
 def run():
  fromaddr =from_generator()+'@'+"".join(fromaddres)
  subject = subject_generator()
@@ -30,19 +37,20 @@ def run():
  IP =  "IP: " + (socket.gethostbyname(socket.gethostname()))
  Date = datetime.now().strftime("%d-%m-%Y_%H_%S")
  toaddrs  = to_generator()+'@'+"".join(toaddreses)
- msg = ("From: %s\r\nSubject: %s\r\nTo: %s\r\n"
+ msg = ("From: %s\r\nSubject: %s\r\nTo: %s\r\nX-Mailer: Smtp Client v.1.0\r\n"
        % (fromaddr, subject,toaddrs))
  msg = msg +IP+"\n"+Date+"\n"+"DATA: "+line
- server = smtplib.SMTP('localhost')
+ server = smtplib.SMTP(parser.get('smtpclient','host'))
  server.set_debuglevel(1)
  server.sendmail(fromaddr,toaddrs,msg)
  server.quit()
 commands = {
     "--start":run,
-    "--help" :help
+    "--help" :help,
+    "--stop" : stop
 }
 if __name__ == "__main__":
    try:
       commands[" ".join(sys.argv[3:])]()
    except KeyError:
-     print "Usage: --start for start service. Or --help for Help."
+     print "Usage: FRom To --start for start service. Or --help for Help."
